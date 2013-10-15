b4f346ffad835d2fe5c886d570c0392c0e4ed041
Phil Gyford <phil@gyford.com>
2013-10-15 11:54:35 +0100

    Improvements, and keeping in sync with py/ru

--- functions.php
+++ functions.php
@@ -2,39 +2,40 @@
 
 $ROOT_DIRECTORY = '/lp-miniseries-example-php/';
 
+// Data for each edition: image filename, description.
 $EDITIONS = array(
-  array('satyre.png', 'Satyre'),
-  array('porcupine.png', 'Porcuspine or Porcupine'),
-  array('lamia.png', 'Lamia'),
-  array('man_ape.png', 'Man Ape'),
-  array('crocodile.png', 'Arabian or Egyptian Land Crocodile'),
-  array('camelopardals.png', 'Camelopardals'),
-  array('boa.png', 'Boa'),
-  array('unicorn.png', 'Unicorn'),
-  array('aegopithecus.png', 'Aegopithecus'),
-  array('badger.png', 'Badger'),
-  array('hydra.png', 'Hydra'),
-  array('ape.png', 'Ape'),
-  array('mantichora.png', 'Mantichora'),
-  array('squirrel.png', 'Squirrel'),
-  array('scythian_wolf.png', 'Scythian Wolf'),
-  array('beaver.png', 'Beaver'),
-  array('cepus_monkey.png', 'Cepus or Martime monkey'),
-  array('mole.png', 'The mole or want'),
-  array('sphinx.png', 'Spinga or Sphinx'),
-  array('bear_ape.png', 'Bear Ape Arctopithecus'),
-  array('cat.png', 'Cat'),
-  array('dragon.png', 'Winged Dragon'),
-  array('prasyan_ape.png', 'Prasyan Ape'),
-  array('su.png', 'A wilde beaste in the New Found World called SU'),
-  array('bear.png', 'Bear'),
-  array('sagoin.png', 'Sagoin, called Galeopithecus'),
-  array('lion.png', 'Lion'),
-  array('another_monster.png', 'Another Monster'),
-  array('adder.png', 'Adder'),
-  array('african_bugil.png', 'African Bugil'),
-  array('allocamelus.png', 'Allocamelus'),
-  array('alpine_mouse.png', 'Alpine Mouse')
+	array('satyre.png', 'Satyre'),
+	array('porcupine.png', 'Porcuspine or Porcupine'),
+	array('lamia.png', 'Lamia'),
+	array('man_ape.png', 'Man Ape'),
+	array('crocodile.png', 'Arabian or Egyptian Land Crocodile'),
+	array('camelopardals.png', 'Camelopardals'),
+	array('boa.png', 'Boa'),
+	array('unicorn.png', 'Unicorn'),
+	array('aegopithecus.png', 'Aegopithecus'),
+	array('badger.png', 'Badger'),
+	array('hydra.png', 'Hydra'),
+	array('ape.png', 'Ape'),
+	array('mantichora.png', 'Mantichora'),
+	array('squirrel.png', 'Squirrel'),
+	array('scythian_wolf.png', 'Scythian Wolf'),
+	array('beaver.png', 'Beaver'),
+	array('cepus_monkey.png', 'Cepus or Martime monkey'),
+	array('mole.png', 'The mole or want'),
+	array('sphinx.png', 'Spinga or Sphinx'),
+	array('bear_ape.png', 'Bear Ape Arctopithecus'),
+	array('cat.png', 'Cat'),
+	array('dragon.png', 'Winged Dragon'),
+	array('prasyan_ape.png', 'Prasyan Ape'),
+	array('su.png', 'A wilde beaste in the New Found World called SU'),
+	array('bear.png', 'Bear'),
+	array('sagoin.png', 'Sagoin, called Galeopithecus'),
+	array('lion.png', 'Lion'),
+	array('another_monster.png', 'Another Monster'),
+	array('adder.png', 'Adder'),
+	array('african_bugil.png', 'African Bugil'),
+	array('allocamelus.png', 'Allocamelus'),
+	array('alpine_mouse.png', 'Alpine Mouse')
 );
 
 
@@ -42,6 +43,7 @@ $EDITIONS = array(
 function display_sample() {
 	global $ROOT_DIRECTORY, $EDITIONS;
 
+	// We can choose which edition we want as the sample:
 	$delivery_count = 0;
 	$image_name = $EDITIONS[$delivery_count][0];
 	$description = $EDITIONS[$delivery_count][1];
@@ -66,23 +68,22 @@ function display_edition() {
 	}
 
 	if (array_key_exists('local_delivery_time', $_GET)) {
-		$local_delivery_time = $_GET['local_delivery_time'];
+		// local_delivery_time is like '2013-10-16T23:20:30-08:00'.
+		// We strip off the timezone, as we only need to know what day this
+		// date is on.
+		$date = strtotime(substr($_GET['local_delivery_time'], 0, -6));
 	} else {
 		// Default to now.
-		$local_delivery_time = gmdate('Y-m-d\TH:i:s.0+00:00');
+		$date = gmdate();
 	}
-
-	// Trim the final part of the time string, and get the day of week.
-	$day_of_week = date('l', strtotime(substr($local_delivery_time, 0, -6)));
 	
-	if ($day_of_week !== 'Wednesday') {
-		// No content is delivered this day.
-		header('ETag: "' . md5('empty' . gmdate('dmY')) . '"');
-		http_response_code(204);
-
-	} elseif (($delivery_count + 1) > count($EDITIONS)) {
+	if (($delivery_count + 1) > count($EDITIONS)) {
 		// The publication has finished, so unsubscribe this subscriber.
 		http_response_code(410);
+
+	} else if (date('l', $date) !== 'Wednesday') {
+		// No content is delivered this day.
+		http_response_code(204);
 
 	} else {
 		// It's all good, so display the publication.
